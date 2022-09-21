40164f3484411d1cf42ca7891b154ad418a4abdb
Markel Rodriguez <rodriguez.markel@uni.eus>
2022-09-21 10:42:44 +0200

    I/OStream ariketak bukatuta

---
+++ 1-Fitxategiak/iostreams/src/main/java/dambi/CopyBytesFNEKontrolatuz.java
@@ -0,0 +1,34 @@
+package dambi;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyBytesFNEKontrolatuz {
+    public static void main(String[] args) throws IOException {
+        FileInputStream in = null;
+        FileOutputStream out = null;
+
+        try {
+            in = new FileInputStream("xanadu.txt");
+            out = new FileOutputStream("outagain.txt");
+            int c;
+
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+        } catch(FileNotFoundException e){
+            System.out.println("ez da existitzen fitxategi hori");
+        }
+        finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+}
+
