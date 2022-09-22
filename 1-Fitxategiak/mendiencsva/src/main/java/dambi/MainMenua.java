62af9d1f25925917b5a0389d7c9dace5816ab08b
Markel Rodriguez <rodriguez.markel@uni.eus>
2022-09-22 13:08:10 +0200

    mendiencsva

---
+++ 1-Fitxategiak/mendiencsva/src/main/java/dambi/MainMenua.java
@@ -0,0 +1,51 @@
+package dambi;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainMenua 
+{
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        int aukera = 0;
+        do {
+            System.out.println();
+            System.out.println("MENUAREN IZENBURUA");
+            System.out.println("====================================");
+            System.out.println("1.- Mendien zerrenda");
+            System.out.println("2.- Mendirik altuena bistaratu");
+            System.out.println("3.- Mendiak esportatu ");
+            System.out.println("4.- ...");
+            System.out.println("5.- Irten");
+            System.out.println("");
+            System.out.print("Aukeratu zenbaki bat: ");
+            aukera = in.nextInt();
+            switch (aukera) {
+                case 1:
+                    met1();
+                    break;
+                case 2:
+                    met2();
+                    break;
+                
+                case 5:
+                    System.out.println("Eskerrik asko programa hau erabiltzeagatik.");
+                    break;
+                default:
+                    System.out.println("Aukera okerra. Saiatu berriz.");
+            }
+        } while (aukera != 5);
+        in.next();
+}
+
+public static void met1(){
+    
+}
+
+public static void met2(){
+   
+}
+}
